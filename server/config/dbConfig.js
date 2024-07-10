a2fafb0f443181a9478c4db7a86c1605811f120d
Keerthivaasan sk <keerthivaasansk@gmail.com>
2024-07-10 20:33:00 +0530

    monodb connected

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,15 +1,23 @@
 const mongoose = require('mongoose');
-mongoose.set('strictQuery', false)
-mongoose.connect(process.env.mongo_url)
+mongoose.set('strictQuery', false); // Not typically necessary for MongoDB connection
+
+const mongoURI = process.env.mongo_url;
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 const connection = mongoose.connection;
 
-connection.on('connected' , ()=>{
-    console.log('Mongo DB Connection Successfull');
-})
+connection.on('connected', () => {
+  console.log('MongoDB Connection Successful');
+});
 
-connection.on('error' , (err)=>{
-    console.log('Mongo DB Connection Failed');
-})
+connection.on('error', (err) => {
+  console.error('MongoDB Connection Error:', err);
+});
 
 module.exports = connection;
